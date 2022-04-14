f703bc5dcf66436473a2668999e6c07954cc6201
Gystre <gystre@gmail.com>
2022-04-13 18:55:45 -0700

    move local env vars to it's own file

--- web/next.config.js
+++ web/next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-
-    env: {
-        // for testing docker image
-        // NEXT_PUBLIC_SERVER_URL: "http://localhost:8080",
-        // NEXT_PUBLIC_API_URL: "http://localhost:8080/graphql",
-
-        NEXT_PUBLIC_SERVER_URL: "http://localhost:3000",
-        NEXT_PUBLIC_API_URL: "http://localhost:3000/graphql",
-    },
 };
 
 module.exports = nextConfig;
